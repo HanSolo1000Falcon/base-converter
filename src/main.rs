eb01e92534767d5670d7795618b474853cf85de9
HanSolo1000Falcon <HanSolo1000Falcon@gmail.com>
2026-04-16 15:50:17 +0200

    Rust is bad

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use std::io;
 const ZERO_CHAR_OFFSET: u8 = '0' as u8;
 const A_CHAR_OFFSET: u8 = 'A' as u8;
 
-/*fn convert(base: u8, current_base: u8, to_convert: &[u8]) {
-
-}*/
+fn convert(base: &u8, current_base: &u8, to_convert: &[u8]) -> String {
+    let mut reconstructed: u64 = 0;
+    for (index, value) in to_convert.iter().enumerate() {
+        let exp = (to_convert.len() - 1 - index) as u32;
+        reconstructed += (*value as u64) * (*current_base as u64).pow(exp);
+    }
+    
+    return reconstructed.to_string();
+}
 
 fn arrayify(to_make: String) -> Vec<u8> {
     let mut arrayified = vec![];
@@ -38,10 +44,18 @@ fn main() {
         println!("Enter the number you want to convert:");
         io::stdin().read_line(&mut to_convert).expect("Failed to read line!");
 
+        let base_u8 = base.parse::<u8>().expect("Failed to parse target base!");
+        let current_base_u8 = current_base.parse::<u8>().expect("Failed to parse base of input number!");
+
         let arrayified = arrayify(to_convert.trim().to_uppercase().to_string());
 
-        for thing in arrayified {
-            println!("{}", thing);
+        for num in &arrayified {
+            if num >= &current_base_u8 {
+                println!("Input base not correct!");
+                continue;
+            }
         }
+
+        println!("{} in base {} converted to {} in base {}", to_convert, current_base_u8, convert(&base_u8, &current_base_u8, &arrayified), base_u8);
     }
 }
